ffa0d42b3fd83b4dcc1eb08090b453db05952236
Grupo - Aitor Calle Rodriguez <acaller08@informatica.iesvalledeljerteplasencia.es>
2025-10-06 20:49:00 +0200

    Ejercicio 9 medio hecho

---
+++ Tema-3/Ejercicio9.Tema3/src/ejercicio9/tema3/Ejercicio9Tema3.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio9.tema3;
+import java.util.Scanner;
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio9Tema3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      Scanner entrada = new Scanner (System.in);
+      
+      int numero1;
+        System.out.println("Por favor, introduzca el primer numero:");
+        numero1 = entrada.nextInt();
+      int numero2;
+        System.out.println("Ahora, introduzca un segundo numero:");
+        numero2 = entrada.nextInt();
+      int numero3;
+        System.out.println("Introduzca el tercer numero:");
+        numero3 = entrada.nextInt();
+      int numero4;
+        System.out.println("Por ultimo, introduzca un cuarto numero:");
+        numero4 = entrada.nextInt();
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
