0da54faf198c47f145a506167d65c857fdba6054
Grupo - Aitor Calle Rodriguez <acaller08@informatica.iesvalledeljerteplasencia.es>
2025-10-06 20:28:50 +0200

    Ejercicio1 Tema 3

---
+++ Tema-3/Ejercicio1.Tema3/src/ejercicio1/tema3/Ejercicio1Tema3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1.tema3;
+import java.util.Scanner;
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio1Tema3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner (System.in);
+        double numero;
+        System.out.println("Por favor, introduzca un numero:");
+        numero = entrada.nextDouble();
+        
+        if (numero > 0){
+        System.out.println("El numero introducido es positivo");
+        }
+        else {
+        System.out.println("El numero introducido es negativo");
+        }
+            
+    }    
+}
