086ee4c787f2c22fbd03825097db367018bcda92
Grupo - Aitor Calle Rodriguez <acaller08@informatica.iesvalledeljerteplasencia.es>
2025-10-09 16:07:54 +0200

    Ejercicio 26 hecho

--- Tema-2/Ejercicio26/src/ejercicio26/Ejercicio26.java
+++ Tema-2/Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -16,20 +16,21 @@ public class Ejercicio26 {
     public static void main(String[] args) {
         // TODO code application logic here
     Scanner entrada = new Scanner (System.in);
-    int xyzw;
-    int x;
-    int y;
-    int z;
-    int w;
+    int xyzw; //Variable para el numero de 4 cifras obtenido
+    int x; //Variable para la 1º cifra
+    int y; //Variable para la 2º cifra
+    int z; //Variable para la 3º cifra
+    int w; //Variable para la 4º cifra
    
     
     
         System.out.println("Introduzca un numero de 4 cifras:");
         xyzw = entrada.nextInt();
-        x = xyzw / 1000;
-        y = (xyzw / 100) % 10;
-        z = (xyzw / 10) % 10;
-        w = xyzw % 10;
+        //Descomponemos el numero en cifras
+        x = xyzw / 1000; //Obtengo la 1º cifra dividiendo el numero entre 1000
+        y = (xyzw / 100) % 10; //Obtengo la 2º cifra dividiendo el numero entre 100 y tomando el resto entre 10
+        z = (xyzw / 10) % 10; //Obtengo la 3º cifra dividiendo el numero entre10 y tomando el resto entre 10
+        w = xyzw % 10; //Obtengi la 4º cifra tomando el resto entre 10
         
         System.out.println("La primera cifra es: " + x);
         System.out.println("La segunda cifra es: " + y);
