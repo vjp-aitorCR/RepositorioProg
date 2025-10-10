cecd272397c53bf5383eb376b9782870d1650265
Grupo - Aitor Calle Rodriguez <acaller08@informatica.iesvalledeljerteplasencia.es>
2025-10-10 18:03:12 +0200

    Ejercicio 16 medio hecho

---
+++ Tema-3/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio16;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador = 0;   //Variable
+        int i;
+        
+        System.out.println("Los numeros impares existentes entre el numero 20 y el 160 son:");
+        
+        for (i = 21; i <= 100; i++)    //Empezamos en 21 porque veinte es par
+            if (i % 2 != 0) {
+                System.out.println(i + " - ");
+                contador++;   
+            }
+        }
+        System.out.println("La cantidad de numeros impares impresos han sido: " + contador); 
+    }
+    
+
