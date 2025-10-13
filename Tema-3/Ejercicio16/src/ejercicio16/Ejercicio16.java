a6c6c54c4df87b49543eedfd02b18441487805e9
vjp-aitorCR <acaller08@informatica.iesvalledeljerteplasencia.es>
2025-10-13 19:32:01 +0200

    Ejercicio 16 hecho

--- Tema-3/Ejercicio16/src/ejercicio16/Ejercicio16.java
+++ Tema-3/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -15,18 +15,19 @@ public class Ejercicio16 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int contador = 0;   //Variable
+        int contador = 0;   //Variable para contar los numeros impares
         int i;
         
-        System.out.println("Los numeros impares existentes entre el numero 20 y el 160 son:");
+        System.out.println("Los numeros impares existentes entre el numero 20 y el 160 son: ");
         
-        for (i = 21; i <= 100; i++)    //Empezamos en 21 porque veinte es par
-            if (i % 2 != 0) {
-                System.out.println(i + " - ");
-                contador++;   
-            }
+        
+        for (i = 21; i <= 160; i += 2) {    //Empezamos en el 21 porque el 20 es par 
+            System.out.print(i + " - ");
+            contador++; //Incrementamos el contador en 1 por cada numero impreso
         }
-        System.out.println("La cantidad de numeros impares impresos han sido: " + contador); 
+
+        System.out.println("La cantidad de numeros impares impresos ha sido: " + contador);
     }
-    
+}
+
 
